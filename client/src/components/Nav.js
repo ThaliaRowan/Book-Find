10a9c453be3fecf1978fd6846cca9e52888e39a8
ThaliaRowan <thalia.rowan@gmail.com>
2021-01-07 21:08:19 -0400

    first commit

---
+++ client/src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style.css";
+
+function Nav(){
+    return(
+        <nav className="navbar navbar-expand-lg navbar-dark">
+      <a className="navbar-brand" href="/">
+        Book Find
+      </a>
+    </nav>
+    );
+}
+
+export default Nav;
---
+++ client/src/pages/Books.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import API from "../utils/API";
+
+function Books(){
+  return (
+    <p>Hey</p>
+  )
+}
+
+export default Books;
